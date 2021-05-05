035f8cc7c109d5638b3581704ae4ed7f91b7b59a
rudziak <artsiom.rudziak@instinctools.com>
2021-05-05 16:08:49 +0300

    change output text after batch

--- src/main/java/com/rudziak/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/com/rudziak/ipldashboard/data/JobCompletionNotificationListener.java
@@ -22,8 +22,8 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("Batch job is finished! Verifying...");
 
-            jdbcTemplate.query("SELECT team1, team2, date FROM match",
-                (rs, row) -> "Team 1 " + rs.getString(1) + " Team 2 " + rs.getString(2) + " Date " + rs.getString(3))
+            jdbcTemplate.query("SELECT team1, team2, date FROM match LIMIT 1",
+                (rs, row) -> "Team 1: " + rs.getString(1) + ", Team 2: " + rs.getString(2) + ", Date: " + rs.getString(3))
                 .forEach(System.out::println);
         }
     }
