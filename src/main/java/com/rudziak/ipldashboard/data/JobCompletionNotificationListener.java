2e1faec28992f808ed83f8fdd9745d002c583a01
artsiom.rudziak <rudziakartsiom@gmail.com>
2021-05-08 17:10:00 +0300

    populate Team table with data

--- src/main/java/com/rudziak/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/com/rudziak/ipldashboard/data/JobCompletionNotificationListener.java
@@ -1,13 +1,17 @@
 package com.rudziak.ipldashboard.data;
 
+import com.rudziak.ipldashboard.model.Team;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -15,16 +19,44 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    private final EntityManager em;
 
     @Override
     public void afterJob(JobExecution jobExecution) {
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("Batch job is finished! Verifying...");
 
-            jdbcTemplate.query("SELECT team1, team2, date FROM match",
-                (rs, row) -> "Team 1 " + rs.getString(1) + " Team 2 " + rs.getString(2) + " Date " + rs.getString(3))
-                .forEach(System.out::println);
+            final Map<String, Team> teamData = new HashMap<>();
+
+            em.createQuery("select m.team1, count(m) from Match m group by m.team1", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .map(m -> new Team((String) m[0], (int) m[1]))
+                    .forEach(team -> teamData.put(team.getTeamName(), team));
+
+            em.createQuery("select m.team2, count(m) from Match m group by m.team2", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .map(m -> new Team((String) m[0], (int) m[1]))
+                    .forEach(team -> {
+                        if (teamData.containsKey(team.getTeamName())) {
+                            final Team exTeam = teamData.get(team.getTeamName());
+                            exTeam.increaseTotalMatchesCount(team.getTotalMatches());
+                        } else {
+                            teamData.put(team.getTeamName(), team);
+                        }
+                    });
+
+            em.createQuery("select m.matchWinner, count(m) from Match m group by m.matchWinner", Object[].class)
+                    .getResultList()
+                    .forEach(objects -> {
+                        final String matchWinner = (String) objects[0];
+                        final int count = (int) objects[1];
+                        teamData.get(matchWinner)
+                                .setTotalWins(count);
+                    });
+
+            teamData.values().forEach(em::persist);
         }
     }
 }
--- src/main/java/com/rudziak/ipldashboard/model/Team.java
+++ src/main/java/com/rudziak/ipldashboard/model/Team.java
@@ -1,14 +1,30 @@
 package com.rudziak.ipldashboard.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Team {
+
+    public Team(String teamName, Integer totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
 
     @Id
     private Long id;
     private String teamName;
-    private Integer totalMathces;
+    private Integer totalMatches;
     private Integer totalWins;
+
+    public void increaseTotalMatchesCount(int delta) {
+        this.totalMatches = this.totalMatches + delta;
+    }
 }
