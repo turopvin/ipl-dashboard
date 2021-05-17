d1a6f762e2a2c860d99f43d23ff2f85a6a3292e2
rudziak <artsiom.rudziak@instinctools.com>
2021-05-17 08:56:04 +0300

    change api

--- src/main/java/com/rudziak/ipldashboard/api/TeamController.java
+++ src/main/java/com/rudziak/ipldashboard/api/TeamController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/teams")
+@RequestMapping("/team")
 @RequiredArgsConstructor
 public class TeamController {
 
