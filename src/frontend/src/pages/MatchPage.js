b937b68e8f7b7bb57e28c1641a4963645f35d6ee
rudziak <artsiom.rudziak@instinctools.com>
2021-05-21 15:59:05 +0300

    add MatchPage.js

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,12 @@
+import {React} from "react";
+import {MatchDetailCard} from "../components/MatchDetailCard";
+import {MatchSmallCard} from "../components/MatchSmallCard";
+
+export const MatchPage = () => {
+
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+        </div>
+    );
+}
