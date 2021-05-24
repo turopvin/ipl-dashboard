f32356aa3a69dd34fd3b75413288d4c7058cbf70
rudziak <artsiom.rudziak@instinctools.com>
2021-05-24 14:28:14 +0300

    styling teampage

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -8,7 +8,7 @@ import "./TeamPage.scss"
 export const TeamPage = () => {
 
     const [team, setTeam] = useState({matches: []});
-    const { teamName } = useParams();
+    const {teamName} = useParams();
 
     useEffect(
         () => {
@@ -28,11 +28,20 @@ export const TeamPage = () => {
 
     return (
         <div className="TeamPage">
-            <div>
-
-            </div><h1>{team.teamName}</h1>
-            <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+            <div className="team-name-section">
+                <h1 className="team-name">{team.teamName}</h1>
+            </div>
+            <div className="win-loss-section">
+                Wins / Losses
+            </div>
+            <div className="match-detail-section">
+                <h3>Latest Matches</h3>
+                <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+            </div>
             {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}
+            <div>
+                <a href="#">More</a>
+            </div>
         </div>
     );
 }
